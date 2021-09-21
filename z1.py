a88dd827877050385d3ceb093b65c2be318a6406
Osichkina Elena <osichkina_elena@mail.ru>
2021-09-21 18:34:22 +0300

    correction

--- z1.py
+++ z1.py
@@ -1,15 +1,10 @@
 import requests
-def determine_smartest():
+def determine_smartest(heroes):
     dict_intelligence = {}
-    list_ = []
-    url_hulk = "https://superheroapi.com/api/2619421814940190/332/powerstats/get"
-    resp_hulk = requests.get(url_hulk).json()
-    dict_intelligence[int(resp_hulk["intelligence"])] = "Hulk"
-    url_thanos = "https://superheroapi.com/api/2619421814940190/655/powerstats/get"
-    resp_thanos = requests.get(url_thanos).json()
-    dict_intelligence[int(resp_thanos["intelligence"])] = "Thanos"
-    url_captain_america = "https://superheroapi.com/api/2619421814940190/149/powerstats/get"
-    resp_captain_america = requests.get(url_captain_america).json()
-    dict_intelligence[int(resp_captain_america["intelligence"])] = "Captain America"
+    for hero in heroes:
+        url_id = f"https://superheroapi.com/api/2619421814940190/search/{hero}"
+        resp = requests.get(url_id).json()
+        iq = resp["results"]
+        dict_intelligence[int(iq[0]['powerstats']["intelligence"])] = hero
     print(f"Самый умный супергерой: {dict_intelligence[max(dict_intelligence)]}")
-determine_smartest()
+determine_smartest(["Hulk", "Captain America","Thanos"])
